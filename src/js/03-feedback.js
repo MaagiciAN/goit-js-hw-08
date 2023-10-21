b0052a26bcb40abf6d411d79651e72d028f63c95
Bohdan Yevsiienko <vad99992@gmail.com>
2023-10-22 01:12:12 +0300

    3rd done, end of 8hw

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from "lodash.throttle";
 
+const elements = {
+    form: document.querySelector('.feedback-form')
+}
+const LOCAL_KEY = "feedback-form-state";
+let localStorageValue = {};
+
+elements.form.addEventListener('submit', onSubmit);
+elements.form.addEventListener('input', throttle(onInput, 500));
+
+refreshForm();
+function refreshForm() {
+    try {
+        const saveData = localStorage.getItem(LOCAL_KEY);
+        if (!saveData) return;
+        localStorageValue = JSON.parse(saveData);
+        Object.entries(localStorageValue).forEach(([key, val]) => {
+            elements.form.elements[key].value = val;
+        })
+    }
+    catch ({ message }) {
+        console.log(message);
+    }
+}
+
+function onInput(event) {
+    const { name, value } = event.target;
+    localStorageValue[name] = value.trim();
+    localStorage.setItem(LOCAL_KEY, JSON.stringify(localStorageValue))
+}
+function onSubmit(event) {
+    event.preventDefault();
+    console.log(localStorageValue);
+    localStorageValue = {};
+    localStorage.removeItem(LOCAL_KEY);
+    elements.form.reset();
+}
